ec3f0c8a2b9c5c55089b4a784db7172ac1e05464
Brahm <brahm.kelly@gmail.com>
2022-05-23 11:28:59 -0400

    added routing/nav

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import './App.css';
-import RestaurantList from './components/RestaurantList';
-import MenuBar from './components/MenuBar';
-import {Layout, Menu} from 'antd'
-import { PlusCircleOutlined, UserOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//Routes is like a switch/case statement, Route is the case
+import RestaurantList from "./components/RestaurantList";
+import MenuBar from "./components/MenuBar";
+import { Layout, Menu } from "antd";
+import {
+  PlusCircleOutlined,
+  UserOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
 
-const {Header , Content} = Layout
-
+const { Header, Content } = Layout;
 
 function App() {
   return (
-   <>
-   <Layout className='layout'>
-     <Header>
-       <MenuBar/>
-
-     </Header>
-     <Content>
-     <RestaurantList />
-     </Content>
-   
-     </Layout>
-   
-   
-   
-   </>
+    <>
+      <BrowserRouter>
+        <Layout className="layout">
+          <Header>
+            <MenuBar />
+          </Header>
+          <Content>
+            <Routes>
+            <Route path='add' element={<h1>Add Restaurant</h1>}/>
+            <Route path='random' element={<h1>Random</h1>}/>
+            <Route path='login' element={<h1>Login</h1>}/>
+              <Route path="/" element={<RestaurantList />} />
+            
+             
+            </Routes>
+          </Content>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import {Layout, Menu} from 'antd'
-import { PlusCircleOutlined, UserOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { useNavigate } from "react-router-dom";
+import { PlusCircleOutlined, UserOutlined, QuestionCircleOutlined, HomeOutlined } from '@ant-design/icons';
+
+
+
 export default function MenuBar(){
+
+    let navigate = useNavigate();
     return (<>
     <Menu theme='dark' mode='horizontal'>
-       <Menu.Item key='add' icon={<PlusCircleOutlined style={{ fontSize: '1.8em'}}/>}></Menu.Item>
+       <Menu.Item  onClick={() => navigate('add')} key='add' icon={<PlusCircleOutlined style={{ fontSize: '1.8em'}}/>}></Menu.Item>
        <Menu.Item key='random' icon={<QuestionCircleOutlined style={{ fontSize: '1.8em'}}/>}></Menu.Item>
-       <Menu.Item key='user' icon={<UserOutlined style={{ fontSize: '1.8em'}}/>}></Menu.Item>
+       <Menu.Item onClick={() => navigate('login')} key='user' icon={<UserOutlined style={{ fontSize: '1.8em'}}/>}></Menu.Item>
+       <Menu.Item onClick={() => navigate('/')} key='home' icon={<HomeOutlined style={{ fontSize: '1.8em'}}/>}></Menu.Item>
+       
        
        </Menu>
     
